844f4e41cc37d6244d00e042395a70d120c642a5
aptheparker <98729115+Aptheparker@users.noreply.github.com>
2022-12-20 15:24:03 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    string s;
+    while (getline(cin, s)) {
+        cout << s << "\n";
+    }
+    return 0;
+}
