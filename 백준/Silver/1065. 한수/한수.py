2cbdc2149b5124345caaf28c5b47284cd504b228
aptheparker <98729115+Aptheparker@users.noreply.github.com>
2022-12-23 11:47:42 +0900

    [Silver IV] Title: 한수, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,10 @@
+n=int(input())
+count=0
+for i in range(1,n+1):
+  if len(str(i))==1 or len(str(i))==2:
+    count+=1
+  else:
+    if int(str(i)[1])-int(str(i)[0]) == int(str(i)[2])-int(str(i)[1]):
+      count+=1
+
+print(count)
